f60838d25bbfe8378a8fed626f6094db9b296572
DAFONTE <DAFONTE@10.0.1.175>
2021-10-25 13:02:32 -0300

    alteração

--- projeto2/src/projeto2/testeaula.java
+++ projeto2/src/projeto2/testeaula.java
@@ -1,5 +1,26 @@
 package projeto2;
 
 public class testeaula {
+	
+	
+	private String  nome;
+	
+	private String sobrenome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
 
 }
