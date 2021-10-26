d35b3bdc7a29bd8fbfbd8b2f4d1d55b75b39941f
DAFONTE <DAFONTE@10.0.1.175>
2021-10-26 13:42:16 -0300

    criação e declaração de variáveis

---
+++ variaveis/src/variaveis/variaveis.java
@@ -0,0 +1,25 @@
+package variaveis;
+
+
+
+public class variaveis {
+	
+	public static void main(String[] args) {
+		
+		int menorIdade = 16;
+		int maiorIdade = 18;
+		int idoso = 60;
+		
+		System.out.println("Menor idade é =" + menorIdade );
+		System.out.println("Maior idade é =" + maiorIdade );
+		System.out.println("Idoso é =" + idoso );
+	}
+	
+	
+	
+
+	
+	
+	
+
+}
